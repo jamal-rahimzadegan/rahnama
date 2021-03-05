5af3c9edea7ff9e02a37a28b8e3090134d869fd4
Jamal Rahimzadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-03-05 22:54:08 +0330

    Update DrawerScreen.js

--- Android/src/component/drawer/DrawerScreen.js
+++ Android/src/component/drawer/DrawerScreen.js
@@ -1,24 +1,3 @@
-import React, {Component} from 'react';
-import {
-    Text,
-    View,
-    Image,
-    ImageBackground,
-    TouchableOpacity,
-    ScrollView,
-    Linking, Share
-} from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
-import styles from '../../assets/styles/DrawerScreen';
-import SInfo from 'react-native-sensitive-info';
-import ContactUsModal from "../modals/ContactUsModal";
-import AboutUsModal from "../modals/AboutUsModal";
-import Icon2 from "react-native-vector-icons/Entypo";
-import Icon3 from "react-native-vector-icons/FontAwesome";
-import Icon4 from "react-native-vector-icons/Ionicons";
-import Icon5 from "react-native-vector-icons/MaterialCommunityIcons";
-import {Context} from "../../context/Context";
-import ThemeModal from "../modals/ThemeModal";
 
 export default class DrawerScreen extends Component {
     static contextType = Context
@@ -42,7 +21,7 @@ export default class DrawerScreen extends Component {
     }
 
     handleSharedPost = postUrl => {
-        if (postUrl.includes('https://www.rahnama.com/item/')) {
+        if (postUrl.includes("deep-linking")) {
             let PostId = postUrl.substring(postUrl.lastIndexOf("/") + 1)
             if (PostId) {
                 this.setState({PostIId: PostId})
@@ -75,7 +54,6 @@ export default class DrawerScreen extends Component {
     render() {
         return (
             <View style={[styles.view_main, {backgroundColor: this.context.BG_COLOR}]}>
-                {/*------Top Image bar---------------------------------------------------------*/}
                 <ImageBackground blurRadius={.8} style={styles.img_background}
                                  source={require('../../assets/img/red_background.png')}>
                     <Image style={styles.arm_style} source={require('../../assets/img/arm.png')}/>
@@ -87,9 +65,7 @@ export default class DrawerScreen extends Component {
                         </TouchableOpacity>
                     }
                 </ImageBackground>
-                {/*------Drawer Btns---------------------------------------------------------*/}
                 <ScrollView>
-                    {/*------Profile---------------------------------------------------------*/}
                     {this.context.token ? (
                         <TouchableOpacity style={styles.EachBtn}
                                           onPress={this.navigateToScreen('Profile')}>
@@ -97,7 +73,6 @@ export default class DrawerScreen extends Component {
                             <Icon3 name={'user'} color={this.context.FONT_COLOR} size={30}/>
                         </TouchableOpacity>
                     ) : null}
-                    {/*-------Inbox messages-------------------------------------------------*/}
                     {this.context.token ?
                         <TouchableOpacity style={styles.EachBtn}
                                           onPress={this.navigateToScreen('Chats')}>
@@ -105,49 +80,34 @@ export default class DrawerScreen extends Component {
                             <Icon2 name={'chat'} color={this.context.FONT_COLOR} size={27}/>
                         </TouchableOpacity>
                         : null}
-                    {/*-------Offers-------------------------------------------------*/}
-                    {/*{this.context.token ?*/}
-                    {/*    <TouchableOpacity style={styles.EachBtn}*/}
-                    {/*        onPress={this.navigateToScreen('Offers')}>*/}
-                    {/*        <Text style={[styles.texts_style,{color: this.context.FONT_COLOR}]}>پیشنهادات قیمت</Text>*/}
-                    {/*        <Icon3 name={'hand-holding-usd'} color={this.context.FONT_COLOR} size={27}/>*/}
-                    {/*    </TouchableOpacity>*/}
-                    {/*    : null}*/}
-                    {/*-------magazine-------------------------------------------------*/}
                     <TouchableOpacity style={styles.EachBtn}
                                       onPress={() => Linking.openURL("http://mag.rahnama.com/")}>
                         <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>مجلات</Text>
                         <Icon5 name={'newspaper'} color={this.context.FONT_COLOR} size={27}/>
                     </TouchableOpacity>
-                    {/*-------Prices-------------------------------------------------*/}
                     <TouchableOpacity style={styles.EachBtn}
                                       onPress={() => Linking.openURL("https://rahnama.com/guest/page/6/prices")}>
                         <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>تعرفه ها</Text>
                         <Icon2 name={'price-tag'} color={this.context.FONT_COLOR} size={27}/>
                     </TouchableOpacity>
-                    {/*-------theme (dark/light)-------------------------------------------------*/}
                     <TouchableOpacity style={styles.EachBtn} onPress={() => this.setState({ThemeModal: true})}>
                         <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>حالت شب</Text>
                         <Icon2 name={'light-bulb'} color={this.context.FONT_COLOR} size={27}/>
                     </TouchableOpacity>
-                    {/*-------contact_us-------------------------------------------------*/}
                     <TouchableOpacity style={styles.EachBtn} onPress={() => this.setState({contact_us: true})}>
                         <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>ارتباط با ما</Text>
                         <Icon4 name={'md-contacts'} color={this.context.FONT_COLOR} size={29}/>
                     </TouchableOpacity>
-                    {/*-------about_us-------------------------------------------------*/}
                     <TouchableOpacity style={styles.EachBtn}
                                       onPress={() => this.setState({about_us: true})}>
                         <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>درباره راهنما</Text>
                         <Icon5 name={'information'} color={this.context.FONT_COLOR} size={29}/>
                     </TouchableOpacity>
-                    {/*-------lists of the Offices-------------------------------------------------*/}
                     <TouchableOpacity style={styles.EachBtn}
                                       onPress={this.navigateToScreen('Offices')}>
                         <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>دفاتر قبول آگهی</Text>
                         <Icon3 name={'building'} color={this.context.FONT_COLOR} size={27}/>
                     </TouchableOpacity>
-                    {/*-------Invite -------------------------------------------------*/}
                     <TouchableOpacity style={styles.EachBtn} onPress={() => {
                         let shareText = "نیازمندی های همشهری\n\https://www.rahnama.com"
                         Share.share({message: shareText})
@@ -155,7 +115,6 @@ export default class DrawerScreen extends Component {
                         <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>معرفی به دوستان</Text>
                         <Icon2 name={'slideshare'} color={this.context.FONT_COLOR} size={27}/>
                     </TouchableOpacity>
-                    {/*-------signOut-------------------------------------------------*/}
                     {this.context.token ?
                         <TouchableOpacity style={styles.EachBtn} onPress={this.signOut.bind(this)}>
                             <Text style={[styles.texts_style, {color: this.context.FONT_COLOR}]}>خروج از حساب
@@ -165,12 +124,9 @@ export default class DrawerScreen extends Component {
                 </ScrollView>
                 <Text
                     style={[styles.VersionTXT, {color: this.context.FONT_COLOR}]}> نسخه: {this.context.appVersion} </Text>
-                {/*------Contact us modal-------------------------------------------------------------------------------*/}
                 <ContactUsModal Visible={this.state.contact_us} Close={() => this.setState({contact_us: false})}/>
-                {/*------Theme Modal -----------------------------------------------------------------------------------*/}
                 <ThemeModal navigation={this.props.navigation} Visible={this.state.ThemeModal}
                             Close={() => this.setState({ThemeModal: false})}/>
-                {/*------about_us modal ------------------------------------------------------------*/}
                 <AboutUsModal Visible={this.state.about_us} Close={() => this.setState({about_us: false})}/>
             </View>
         );
@@ -184,7 +140,7 @@ export default class DrawerScreen extends Component {
 
     signOut = async () => {
         try {
-            await SInfo.setItem('@TOKEN', '', {});
+            await SInfo.setItem('user token goes here', '', {});
             this.context.setToken('')
             AsyncStorage.removeItem('@PHONE');
             this.setState({
